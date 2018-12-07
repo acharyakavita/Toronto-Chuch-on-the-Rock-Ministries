e774362b40dab37b15c189bc6fddbd1072a253ee
acharyakavita <acharyakavita12@gmail.com>
2018-12-07 17:06:39 -0500

    Added contact.html page Made the website responsive.

--- js/app.js
+++ js/app.js
@@ -2,19 +2,22 @@
 
 /*On page load Verse of the day will be loaded from 'www.ourmanna.com'*/
 window.onload = (function () {
-    /*Verse api call*/
-    $.ajax({
-        url: "https://cors-anywhere.herokuapp.com/http://www.ourmanna.com/verses/api/get/?format=json",
-        dataType: "json"
-    }).done(addVerse)
-        .fail(error)
+    let verse = document.querySelector('.verse');
+    if (verse) {
+        /*Verse api call*/
+        $.ajax({
+            url: "https://cors-anywhere.herokuapp.com/http://www.ourmanna.com/verses/api/get/?format=json",
+            dataType: "json"
+        }).done(addVerse)
+            .fail(error)
+    }
 })();
 
 /*Error function if API call fails*/
 function error() {
     console.log('Verse of the day call failed.So default verse will be displayed');
     let verse = document.querySelector('.verse');
-    verse.innerHTML=`<h4>Verse of the Day</h4><h5 class="verse-author">John 3:16</h5>
+    verse.innerHTML = `<h4>Verse of the Day</h4><h5 class="verse-author">John 3:16</h5>
                     <p class="verse-content">For God so loved the world that he gave his one and only Son,
                     that whoever believes in him shall not perish but have eternal life.</p>`
 }
@@ -26,3 +29,22 @@ function addVerse(verse) {
     author.textContent = verse.verse.details.reference;
     content.textContent = verse.verse.details.text;
 }
+
+
+let menu = (function () {
+    let hamburger = document.querySelector('.ham-icon');
+    hamburger.addEventListener('click', function () {
+        let menu = document.querySelector(".menu-list");
+        let menuList = document.querySelector(".menu-list-click");
+        console.log(menuList)
+        if (menu.style.display === "" && menuList===null) {
+            menu.classList.add('menu-list-click');
+        } 
+        else{
+            menuList.classList.remove('menu-list-click');
+            console.log('jio')
+        }
+    })
+})();
+
+
